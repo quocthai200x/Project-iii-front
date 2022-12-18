3e9447475158dcf6c5309a7657ec41b98d8cf447
Quoc Thai <quocthai2000x@gmail.com>
2022-12-18 20:51:00 +0700

    change back 3

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export default {
     rootPath: process.env.NODE_ENV === 'development'
-    ?'http://localhost:5555':'https://project-iii-back.vercel.app/'
+    ?'http://localhost:5555':'https://back-o0qe.onrender.com/'
     // rootPath: 'http://localhost:6969'
 };
